f3547a92d3fe928725717a9325f290cdb2262b2b
Alex Wade <alex.wade13@gmail.com>
2020-10-15 15:27:06 -0700

    fixed warnings for deploy

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import fetch from 'node-fetch';
 
 
 function App() {
 
-  const [count, setCount] = useState(0);
+  const [zip, setZip] = useState(98028);
 
-  const [hasError, setErrors] = useState(false);
   const [planets, setPlanets] = useState({});
 
   async function fetchData() {
-    const res = await fetch("https://api.openweathermap.org/data/2.5/weather?zip=98028,us&appid=709847967f5e54b97308c1b2cae4dee5");
-    res.json()
-      
+    const res = await fetch("https://api.openweathermap.org/data/2.5/weather?zip=" + zip +",us&appid=709847967f5e54b97308c1b2cae4dee5");
+    res
+      .json()
+      .then(res => setPlanets(res))
   }
 
   useEffect(() => {
-    console.log("test")
     fetchData();
-  });  
+    // eslint-disable-next-line
+  }, []);  
 
   return (
     <div className="App">
       <header className="App-header">
-      <button onClick={() => setCount(count + 1)}>
-        Click me {count}
+      <button onClick={() => setZip(10029)}>
+        Click me {zip}
       </button>       
       {JSON.stringify(planets)}
         
