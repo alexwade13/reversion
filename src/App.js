6c7d562c303170e35efd1a49a754df8f815a6cba
Alex Wade <alex.wade13@gmail.com>
2020-10-15 15:04:45 -0700

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fetch from 'node-fetch';
+
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const [hasError, setErrors] = useState(false);
+  const [planets, setPlanets] = useState({});
+
+  async function fetchData() {
+    const res = await fetch("https://swapi.co/api/planets/4/");
+    res
+      .json()
+      .then(res => setPlanets(res))
+      .catch(err => setErrors(err));
+  }
+
+  useEffect(() => {
+    console.log("test")
+    fetchData();
+  });  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button onClick={() => setCount(count + 1)}>
+        Click me {count}
+      </button>       
+      {JSON.stringify(planets)}
+        
       </header>
+      }
     </div>
   );
 }
